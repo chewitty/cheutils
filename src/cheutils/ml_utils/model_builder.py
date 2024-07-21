ec1d94ee486640854430d8b59898cf4d84473a2b
Ferdinand Che <ferdinand.che@gmail.com>
2024-07-21 13:45:45 +0200

    added coarse_fine_tune method but needs filling out after properties are loaded

--- src/cheutils/ml_utils/model_builder.py
+++ src/cheutils/ml_utils/model_builder.py
@@ -122,6 +122,58 @@ def tune_model(pipeline: Pipeline, X, y, params_grid: dict, cv, grid_search: boo
         return search_cv.best_estimator_, search_cv.best_score_, search_cv.best_params_
     else:
         return search_cv.cv_results_
+
+@track_duration(name='coarse_fine_tune')
+@debug_func(enable_debug=True, prefix='coarse_fine_tune')
+def coarse_fine_tune(pipeline: Pipeline, X, y, params_grid: dict, cv, grid_search: bool = False,
+               scoring: str = "neg_mean_squared_error", random_state=100, debug: bool = False, scan_params: bool=False, **kwargs):
+    assert pipeline is not None, "A valid pipeline instance expected"
+    assert params_grid is not None, "A valid parameter grid (a dict) expected"
+    assert (cv is not None), "A valid cv, either the number of folds or an instance of something like StratifiedKFold"
+    n_iters = 1000
+    n_jobs = -1
+    search_cv = None
+    if grid_search:
+        if debug:
+            search_cv = GridSearchCV(estimator=pipeline, param_grid=params_grid,
+                                     scoring=scoring, cv=cv, n_jobs=n_jobs, verbose=2, error_score="raise",)
+        else:
+            search_cv = GridSearchCV(estimator=pipeline, param_grid=params_grid,
+                                     scoring=scoring, cv=cv, n_jobs=n_jobs,)
+    else:
+        if debug:
+            search_cv = RandomizedSearchCV(estimator=pipeline, param_distributions=params_grid,
+                                           scoring=scoring, cv=cv, n_iter=n_iters, n_jobs=n_jobs,
+                                           random_state=random_state, verbose=2, error_score="raise",)
+        else:
+            search_cv = RandomizedSearchCV(estimator=pipeline, param_distributions=params_grid,
+                                           scoring=scoring, cv=cv, n_iter=n_iters, n_jobs=n_jobs,
+                                           random_state=random_state,)
+    name = None
+    if "name" in kwargs:
+        name = kwargs.get("name")
+        del kwargs["name"]
+    if name is not None:
+        show_pipeline(search_cv, name=name, save_to_file=True)
+    else:
+        show_pipeline(search_cv)
+    search_cv.fit(X, y)
+    if not scan_params:
+        return search_cv.best_estimator_, search_cv.best_score_, search_cv.best_params_
+    else:
+        return search_cv.cv_results_
+
+def get_narrow_param_grid(best_params: dict, factor=2):
+    param_grid = {}
+    for param, value in best_params.items():
+        if isinstance(value, int):
+            param_grid[param] = [value // factor, value, value * factor]
+        elif isinstance(value, float):
+            param_grid[param] = [value / factor, value, value * factor]
+        else:
+            param_grid[param] = [value]
+    return param_grid
+
 @track_duration(name='cv')
 @debug_func(enable_debug=True, prefix='cv')
 def cross_val_model(pipeline: Pipeline, X, y, scoring, cv=5, **fit_params):
