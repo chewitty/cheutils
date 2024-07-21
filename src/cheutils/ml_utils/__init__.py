7a238cad770ce5aacf7c43ea5e3a6703cb556ab2
Ferdinand Che <ferdinand.che@gmail.com>
2024-07-21 16:27:29 +0200

    using the corrected version of model_options with the corrected way hyperparameter types are processed and compared

---
+++ src/cheutils/ml_utils/__init__.py
@@ -0,0 +1,11 @@
+from cheutils.properties_util import PropertiesUtil
+from cheutils.debugger import Debugger
+DEBUGGER = Debugger(enable_debug=True)
+APP_PROPS = PropertiesUtil()
+prop_key = 'project.namespace'
+PROJECT_NAMESPACE = APP_PROPS.get(prop_key)
+assert (PROJECT_NAMESPACE is not None), 'Project namespace property must be specified'
+prop_key = 'project.model.supported'
+MODELS_SUPPORTED = APP_PROPS.get_list(prop_key)
+assert (MODELS_SUPPORTED is not None), 'Models supported must be specified'
+DEBUGGER.debug('Models supported = ', MODELS_SUPPORTED)
