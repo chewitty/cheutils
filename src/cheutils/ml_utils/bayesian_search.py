f99ac111ebeeedf7ac3b80d567900eb58750ad0d
Ferdinand Che <ferdinand.che@gmail.com>
2024-07-22 20:43:04 +0200

    returning fitted estimator from bayesian optimization so it can be used immediately for predicting

--- src/cheutils/ml_utils/bayesian_search.py
+++ src/cheutils/ml_utils/bayesian_search.py
@@ -1,9 +1,12 @@
+import numpy as np
+from cheutils.debugger import Debugger
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 
 from cheutils.ml_utils.model_options import get_regressor
 
+DBUGGER = Debugger()
 class BayesianSearch(object):
     def __init__(self, param_grid: dict, model_option:str=None, **kwargs):
         self.param_grid = param_grid
@@ -16,12 +19,12 @@ class BayesianSearch(object):
         self.cv = kwargs.get('cv', 5)
 
     def fit(self, X, y=None, **kwargs):
-        print('BayesianSearch: Fitting dataset, shape', X.shape, y.shape if y is not None else None)
+        DBUGGER.debug('BayesianSearch: Fitting dataset, shape', X.shape, y.shape if y is not None else None)
         # Define the hyperparameter space
         params_space = {}
         for key, value in self.param_grid.items():
             if isinstance(value[0], int):
-                params_space[key] = hp.quniform(key, value[0], value[-1], 1)
+                params_space[key] = hp.choice(key, np.arange(value[0], value[-1], dtype=int))
             elif isinstance(value[0], float):
                 params_space[key] = hp.uniform(key, value[0], value[-1])
             elif isinstance(value[0], bool):
@@ -35,11 +38,11 @@ class BayesianSearch(object):
             model.fit(X, y)
             y_pred = model.predict(X)
             score = mean_squared_error(y, y_pred)
-            return {'loss': -score, 'status': STATUS_OK}
+            return {'loss': score, 'status': STATUS_OK} # MSE already seeks minimal to optimize - so do not return negative value
         # Perform the optimization
         trials = Trials()
         self.best_params_ = fmin(objective, params_space, algo=tpe.suggest, max_evals=100, trials=trials)
-        print("Best set of hyperparameters: ", self.best_params_)
+        DBUGGER.debug("Best bayesian hyperparameters: ", self.best_params_)
         # fit the model and predict
         self.best_estimator_ = get_regressor(model_option=self.model_option, **self.best_params_)
         self.cv_results_ = cross_val_score(self.best_estimator_, X, y, scoring=self.scoring, cv=self.cv,)
