8b3f9886ae52ef957b8520203481abd649e82f5e
Ferdinand Che <ferdinand.che@gmail.com>
2024-07-22 23:29:56 +0200

    version before migrating to hpsklearn - the hypeopt wrappers for the most popular sklearn functions

--- src/cheutils/ml_utils/bayesian_search.py
+++ src/cheutils/ml_utils/bayesian_search.py
@@ -1,6 +1,7 @@
 import numpy as np
 from cheutils.debugger import Debugger
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+from numpy.random import PCG64
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 
@@ -8,13 +9,15 @@ from cheutils.ml_utils.model_options import get_regressor
 
 DBUGGER = Debugger()
 class BayesianSearch(object):
-    def __init__(self, param_grid: dict, model_option:str=None, **kwargs):
+    def __init__(self, param_grid: dict, model_option:str=None, random_state: int=100, **kwargs):
         self.param_grid = param_grid
         self.model_option = model_option
+        self.random_state = random_state
         self.best_estimator_ = None
         self.best_params_ = None
         self.best_score_ = None
         self.cv_results_ = None
+        self.trials = None
         self.scoring = kwargs.get('scoring', 'neg_mean_squared_error')
         self.cv = kwargs.get('cv', 5)
 
@@ -33,20 +36,21 @@ class BayesianSearch(object):
                 params_space[key] = hp.choice(key, value)
 
         # Define the objective function to minimize
-        def objective(params):
-            model = get_regressor(model_option=self.model_option)
-            model.fit(X, y)
-            y_pred = model.predict(X)
-            score = mean_squared_error(y, y_pred)
-            return {'loss': score, 'status': STATUS_OK} # MSE already seeks minimal to optimize - so do not return negative value
+        def best_mse_by_cv(params):
+            model = get_regressor(model_option=self.model_option, **params, random_state=self.random_state)
+            cv_scores = -cross_val_score(model, X, y, scoring=self.scoring, cv=self.cv, n_jobs=-1)
+            return {'loss': cv_scores.mean(), 'cv_results': cv_scores, 'status': STATUS_OK}
         # Perform the optimization
         trials = Trials()
-        self.best_params_ = fmin(objective, params_space, algo=tpe.suggest, max_evals=100, trials=trials)
+        self.best_params_ = fmin(best_mse_by_cv, params_space, algo=tpe.suggest, max_evals=100,
+                                 trials=trials, rstate=np.random.Generator(PCG64(self.random_state)))
         DBUGGER.debug("Best bayesian hyperparameters: ", self.best_params_)
         # fit the model and predict
-        self.best_estimator_ = get_regressor(model_option=self.model_option, **self.best_params_)
-        self.cv_results_ = cross_val_score(self.best_estimator_, X, y, scoring=self.scoring, cv=self.cv,)
-        self.best_score_ = self.cv_results_.mean()
+        self.best_estimator_ = get_regressor(model_option=self.model_option, **self.best_params_, random_state=self.random_state)
+        best_result = best_mse_by_cv(self.best_params_)
+        self.best_score_ = best_result.get('loss')
+        self.cv_results_ = best_result.get('cv_results')
+        self.trials = trials
         # fit estimator so it can be immediately used for predicting
         self.best_estimator_.fit(X, y)
         return self
