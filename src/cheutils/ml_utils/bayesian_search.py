6b5d46a6f7604b73aea410045ebb62c2617062c2
Ferdinand Che <ferdinand.che@gmail.com>
2024-08-28 19:58:13 +0200

    added HyperoptSearchCV - i.e., including cross_validation

--- src/cheutils/ml_utils/bayesian_search.py
+++ src/cheutils/ml_utils/bayesian_search.py
@@ -1,12 +1,14 @@
 from functools import partial
 
 import numpy as np
-from hyperopt import tpe, hp, mix, anneal, rand
+from numpy.random import RandomState
+from hyperopt import fmin, tpe, hp, mix, anneal, rand, space_eval
 from hyperopt.pyll import scope
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
 from hpsklearn import HyperoptEstimator
 from cheutils.common_base import CheutilsBase
-from cheutils.ml_utils.model_options import get_hyperopt_regressor
+from cheutils.ml_utils.model_options import get_hyperopt_regressor, get_regressor
 from cheutils.loggers import LoguruWrapper
 
 LOGGER = LoguruWrapper().get_logger()
@@ -99,12 +101,75 @@ class HyperoptSearch(CheutilsBase):
         self.best_params_ = self.base_estimator_.get_params()
         self.trials_ = self.best_estimator_.trials
         self.cv_results_ = self.best_estimator_.trials
-        LOGGER.debug("Best bayesian hyperparameters  = {}", self.best_params_)
+        LOGGER.debug('HyperoptSearch: Best hyperparameters  = {}', self.best_params_)
         return self
 
     def predict(self, X):
-        assert X is not None, "A valid X expected"
+        assert X is not None, 'A valid X expected'
         return self.base_estimator_.predict(X)
+
+    def predict_proba(self, X):
+        assert X is not None, 'A valid X expected'
+        return self.base_estimator_.predict_proba(X)
+
+class HyperoptSearchCV(CheutilsBase):
+    def __init__(self, model_option:str=None, max_evals: int=100,
+                 preprocessing: list=None, cv=3, n_jobs: int=-1, param_space: dict= {},
+                 random_state: int=100, trial_timeout: int=60, **kwargs):
+        super().__init__()
+        self.model_option = model_option
+        self.max_evals = max_evals
+        self.cv_ = cv
+        self.n_jobs_ = n_jobs
+        self.random_state = random_state
+        self.best_estimator_ = None
+        self.best_params_ = None
+        self.best_score_ = float('inf')
+        self.cv_results_ = None
+        self.scoring_ = kwargs.get('scoring', 'neg_mean_squared_error')
+        self.params_space_ = param_space
+        self.X = None
+        self.y = None
+
+    def fit(self, X, y=None, **kwargs):
+        LOGGER.debug('HyperoptSearchCV: Fitting dataset, shape {}, {}', X.shape, y.shape if y is not None else None)
+        self.X = X
+        self.y = y
+        # Perform the optimization
+        p_suggest = [(0.05, rand.suggest), (0.75, tpe.suggest), (0.20, anneal.suggest)]
+        mix_algo = partial(mix.suggest, p_suggest=p_suggest)
+        best_params = fmin(fn=self.__objective, space=self.params_space_, algo=mix_algo,
+                           max_evals=self.max_evals, rstate=np.random.default_rng(self.random_state))
+        self.best_params_ = space_eval(self.params_space_, best_params)
+        self.best_estimator_ = get_regressor(**self.__get_model_params(self.best_params_))
+        self.best_estimator_.fit(X, y)
+        LOGGER.debug('HyperoptSearchCV: Best hyperparameters  = {}', self.best_params_)
+        return self
+
+    def predict(self, X):
+        assert X is not None, 'A valid X expected'
+        assert self.best_estimator_ is not None, 'Model must be fitted before predict'
+        return self.best_estimator_.predict(X)
+
+    def predict_proba(self, X):
+        assert X is not None, 'A valid X expected'
+        assert self.best_estimator_ is not None, 'Model must be fitted before predict_proba'
+        return self.best_estimator_.predict_proba(X)
+
+    def __objective(self, params):
+        underlying_model = get_regressor(**self.__get_model_params(params))
+        cv_score = cross_val_score(underlying_model, self.X, self.y, scoring=self.scoring_,
+                                cv=self.cv_, n_jobs=self.n_jobs_)
+        score = abs(cv_score.mean())
+        if score < self.best_score_:
+            self.best_score_ = score
+            self.cv_results_ = cv_score
+        return score
+
+    def __get_model_params(self, params):
+        model_params = params.copy()
+        model_params['model_option'] = self.model_option
+        return model_params
 
 
 
