corrected potential data leakage

src/cheutils/data_prep.py
@@ -6,7 +6,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import RobustScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_selection import RFE
-from cheutils.common_utils import apply_clipping
+from cheutils.common_utils import apply_clipping, parse_special_features
 from cheutils.loggers import LoguruWrapper
 from cheutils.properties_util import AppProperties
 
@@ -14,24 +14,38 @@ LOGGER = LoguruWrapper().get_logger()
 APP_PROPS = AppProperties()
 CONFIG_TRANSFORMERS = APP_PROPS.get_dict_properties('model.selectivescaler.transformers')
 
-class DateTransformer(BaseEstimator, TransformerMixin):
+class DateFeaturesTransformer(BaseEstimator, TransformerMixin):
     """
-    Transforms dates.
+    Transforms datetimes, generating additional prefixed 'dow', 'wk', 'qtr', 'wkend' features for all relevant columns
+    (specified) in the dataframe; drops the datetime column by default but can be retained as desired.
     """
-    def __init__(self, rel_cols: list, prefixes: list, **kwargs):
+    def __init__(self, rel_cols: list, prefixes: list, drop_rel_cols: list=None, **kwargs):
+        """
+        Transforms datetimes, generating additional prefixed 'dow', 'wk', 'qtr', 'wkend' features for all relevant
+        columns (specified) in the dataframe; drops the datetime column by default but can be retained as desired.
+        :param rel_cols: the column labels for desired datetime columns in the dataframe
+        :type rel_cols: list
+        :param prefixes: the corresponding prefixes for the specified datetime columns, e.g., 'date_'
+        :type prefixes: list
+        :param drop_rel_cols: the coresponding list of index matching flags indicating whether to drop the original
+        datetime column or not; if not specified, defaults to True for all specified columns
+        :type drop_rel_cols: list
+        :param kwargs:
+        :type kwargs:
+        """
         super().__init__(**kwargs)
         self.target = None
         self.rel_cols = rel_cols
         self.prefixes = prefixes
-        self.trans_cols = rel_cols
+        self.drop_rel_cols = drop_rel_cols
 
     def fit(self, X, y=None):
-        LOGGER.debug('DateTransformer: Fitting dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
+        LOGGER.debug('DateFeaturesTransformer: Fitting dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
         self.target = y  # possibly passed in chain
         return self
 
     def transform(self, X, y=None):
-        LOGGER.debug('DateTransformer: Transforming dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
+        LOGGER.debug('DateFeaturesTransformer: Transforming dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
         new_X = X.copy(deep=True)
         new_X.reset_index(drop=True, inplace=True)
         # otherwise also generate the following features
@@ -49,8 +63,15 @@ class DateTransformer(BaseEstimator, TransformerMixin):
             new_X.loc[:, prefix + 'wkend'] = np.where(new_X[rel_col].dt.dayofweek.isin([5, 6]), 1, 0)
             new_X[prefix + 'wkend'] = new_X[prefix + 'wkend'].astype(int)
         if len(self.rel_cols) > 0:
-            new_X.drop(columns=self.rel_cols, inplace=True)
-        LOGGER.debug('DateTransformer: Transformed dataset, shape = {}, {}', new_X.shape, y.shape if y is not None else None)
+            if self.drop_rel_cols is None or not self.drop_rel_cols:
+                new_X.drop(columns=self.rel_cols, inplace=True)
+            else:
+                to_drop_cols = []
+                for index, to_drop_cols in enumerate(self.rel_cols):
+                    if self.drop_rel_cols[index]:
+                        to_drop_cols.append(to_drop_cols)
+                new_X.drop(columns=to_drop_cols, inplace=True)
+        LOGGER.debug('DateFeaturesTransformer: Transformed dataset, shape = {}, {}', new_X.shape, y.shape if y is not None else None)
         return new_X
 
     def get_date_cols(self):
@@ -58,7 +79,7 @@ class DateTransformer(BaseEstimator, TransformerMixin):
         Returns the transformed date columns, if any
         :return:
         """
-        return self.trans_cols
+        return self.rel_cols
 
     def get_target(self):
         return self.target
@@ -67,12 +88,26 @@ class SpecialFeaturesTransformer(BaseEstimator, TransformerMixin):
     """
     Transforms dates.
     """
-    def __init__(self, rel_col: str, feature_mappings: dict, **kwargs):
+    def __init__(self, rel_col: str, feature_mappings: dict, sep:str=',', drop_col: bool=True, **kwargs):
+        """
+        Process any special textual features based on the specified feature mappings, using 1/0 flags to indicate any
+        special features included in the dataframe.
+        :param rel_col: the column label with the textual features to be processed
+        :type rel_col:
+        :param feature_mappings: a dictionary of features or tokens to be matched in feature string and their corresponding desired column labels - e.g., {"feat1": "label1", "feat2": "label2", "Trailers": "trailers"}
+        :type feature_mappings:
+        :param sep: the separator character used in the input string; default is ','
+        :type sep:
+        :param drop_col: whether to drop the original column containing the textual features; default is True
+        :param kwargs:
+        :type kwargs:
+        """
         super().__init__(**kwargs)
         self.target = None
         self.rel_col = rel_col
         self.feature_mappings = feature_mappings
-        self.trans_cols = rel_col
+        self.sep = sep
+        self.drop_col = drop_col
 
     def fit(self, X, y=None):
         LOGGER.debug('SpecialFeaturesTransformer: Fitting dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
@@ -83,37 +118,16 @@ class SpecialFeaturesTransformer(BaseEstimator, TransformerMixin):
         LOGGER.debug('SpecialFeaturesTransformer: Transforming dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
         new_X = X.copy(deep=True)
         new_X.reset_index(drop=True, inplace=True)
-
-        def parse_special_features(special_feat_str, feature_mappings: dict):
-            """
-            Returns a dictionary with flags indicating the special features included in the movie
-            :param special_feat_str: any relevant string that may contain some special features to be matched - e.g., "Trailers, commentaries, feat1, feat2,"
-            :type special_feat_str: str
-            :param feature_mappings: complete dictionary of features or tokens to be matched in feature string and their corresponding desired labels - e.g., {"feat1": "label1", "feat2": "label2", "Trailers": "trailers"}
-            :type feature_mappings: dict
-            :return: list of flags indicating the special features, identified by the feature mapping keys, included in the input string correctly matched
-            :rtype: list
-            """
-            assert special_feat_str is not None, "Special features expected"
-            assert feature_mappings is not None, 'Special feature mappings expected'
-            feat_split = special_feat_str.split(",")
-            feat_keys = list(feature_mappings.keys())
-            feat_mappings = list(feature_mappings.values())
-            feat_pattern = {mapping: 0 for mapping in feat_mappings}
-            for feat_key in feat_keys:
-                if feat_key in feat_split:
-                    feat_pattern[feature_mappings.get(feat_key)] = 1
-            return list(feat_pattern.values())
         # otherwise also generate the following features
-        num_mappings = len(self.feature_mappings.values())
-        created_features = new_X[self.rel_col].apply(lambda x: parse_special_features(x, self.feature_mappings))
+        created_features = new_X[self.rel_col].apply(lambda x: parse_special_features(x, self.feature_mappings, sep=self.sep))
         new_feat_values = {mapping: [] for mapping in self.feature_mappings.values()}
         for index, col in enumerate(self.feature_mappings.values()):
             for row in range(created_features.shape[0]):
                 new_feat_values.get(col).append(created_features[row][index])
             new_X.loc[:, col] = new_feat_values.get(col)
         if self.rel_col is not None:
-            new_X.drop(columns=[self.rel_col], inplace=True)
+            if self.drop_col:
+                new_X.drop(columns=[self.rel_col], inplace=True)
         LOGGER.debug('SpecialFeaturesTransformer: Transformed dataset, shape = {}, {}', new_X.shape, y.shape if y is not None else None)
         return new_X
 
@@ -122,7 +136,7 @@ class SpecialFeaturesTransformer(BaseEstimator, TransformerMixin):
         Returns the transformed date columns, if any
         :return:
         """
-        return self.trans_cols
+        return self.rel_col
 
     def get_target(self):
         return self.target
@@ -370,3 +384,110 @@ class SelectiveColumnTransformer(ColumnTransformer):
         transformed_X = super().fit_transform(X, y, **fit_params)
         new_X = pd.DataFrame(transformed_X, columns=self.feature_names)
         return new_X
+
+class DataPrepTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, date_cols: list=None, int_cols: list=None, float_cols: list=None,
+                 masked_cols: dict=None, special_features: dict=None, drop_feats_cols: bool=True,
+                 calc_features: dict=None, gen_target: dict=None, pot_leak_cols: list=None, **kwargs):
+        """
+        Preprocessing dataframe columns to ensure consistent data types and formatting, and optionally extracting any special features described by dictionaries of feature mappings - e.g., special_features = {'col_label1': {'feat_mappings': {'Trailers': 'trailers', 'Deleted Scenes': 'deleted_scenes', 'Behind the Scenes': 'behind_scenes', 'Commentaries': 'commentaries'}, 'sep': ','}, }.
+        :param date_cols: any date columns to be concerted to datetime
+        :type date_cols:
+        :param int_cols: any int columns to be converted to int
+        :type int_cols:
+        :param float_cols: any float columns to be converted to float
+        :type float_cols:
+        :param masked_cols: dictionary of columns and function generates a mask or a mask (bool Series) - e.g., {'col_label1': mask_func)
+        :type masked_cols:
+        :param special_features: dictionaries of feature mappings - e.g., special_features = {'col_label1': {'feat_mappings': {'Trailers': 'trailers', 'Deleted Scenes': 'deleted_scenes', 'Behind the Scenes': 'behind_scenes', 'Commentaries': 'commentaries'}, 'sep': ','}, }
+        :type special_features:
+        :param drop_feats_cols: drop special_features cols if True
+        :param calc_features: dictionary of calculated column labels with their corresponding column generation functions - e.g., {'col_label1': col_gen_func1, 'col_label2': col_gen_func2}
+        :param gen_target: dictionary of target column label and target generation function (e.g., a lambda expression to be applied to rows (i.e., axis=1), such as {'target_col': 'target_collabel', 'target_gen_func': target_gen_func}
+        :param pot_leak_cols: columns that could potentially introduce data leakage and should be dropped
+        :param kwargs:
+        :type kwargs:
+        """
+        self.date_cols = date_cols
+        self.int_cols = int_cols
+        self.float_cols = float_cols
+        self.masked_cols = masked_cols
+        self.feature_mappings = special_features
+        self.drop_feats_cols = drop_feats_cols
+        self.gen_target = gen_target
+        self.calc_features = calc_features
+        self.pot_leak_cols = pot_leak_cols
+
+    def fit(self, X, y=None):
+        LOGGER.debug('DataPrepTransformer: Fitting dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
+        return self
+
+    def transform(self, X, y=None):
+        LOGGER.debug('DataPrepTransformer: Transforming dataset, shape = {}, {}', X.shape, y.shape if y is not None else None)
+        if self.date_cols is not None:
+            for col in self.date_cols:
+                X[col] = pd.to_datetime(X[col])
+        if self.int_cols is not None:
+            for col in self.int_cols:
+                X[col] = X[col].astype(int)
+        if self.float_cols is not None:
+            for col in self.float_cols:
+                X[col] = X[col].astype(float)
+        new_X = X.copy(deep=True)
+        def process_feature(col, feat_mappings, sep:str=','):
+            created_features = new_X[col].apply(lambda x: parse_special_features(x, feat_mappings, sep=sep))
+            new_feat_values = {mapping: [] for mapping in feat_mappings.values()}
+            for index, col in enumerate(feat_mappings.values()):
+                for row in range(created_features.shape[0]):
+                    new_feat_values.get(col).append(created_features[row][index])
+                new_X.loc[:, col] = new_feat_values.get(col)
+        if self.feature_mappings is not None:
+            rel_cols = self.feature_mappings.keys()
+            for col in rel_cols:
+                # first apply any regex replacements to clean-up
+                regex_pat = self.feature_mappings.get(col).get('regex_pat')
+                regex_repl = self.feature_mappings.get(col).get('regex_repl')
+                if regex_pat is not None:
+                    new_X[col] = new_X[col].str.replace(regex_pat, regex_repl, regex=True)
+                # then process features mappings
+                feat_mappings = self.feature_mappings.get(col).get('feat_mappings')
+                sep = self.feature_mappings.get(col).get('sep')
+                process_feature(col, feat_mappings, sep=sep if sep is not None else ',')
+            if self.drop_feats_cols:
+                new_X.drop(columns=rel_cols, inplace=True)
+        # generate any calculated columns as needed
+        if self.calc_features is not None:
+            for col, col_gen_func in self.calc_features.items():
+                new_X[col] = new_X.apply(col_gen_func, axis=1)
+        # apply any masking logic
+        if self.masked_cols is not None:
+            for col, mask in self.masked_cols.items():
+                new_X[col] = np.where(new_X.apply(mask, axis=1), 1, 0)
+        # generate any target variables as needed
+        # do this safely so that if any missing features is encountered, as with real unseen data situation where
+        # future variable is not available at the time of testing, then ignore the target generation as it ought
+        # to be predicted
+        try:
+            if self.gen_target is not None:
+                target_col = self.gen_target.get('target_col')
+                target_gen_func = self.gen_target.get('target_gen_func')
+                new_X[target_col] = new_X.apply(target_gen_func, axis=1)
+        except:
+            pass
+        if self.pot_leak_cols is not None or not (not self.pot_leak_cols):
+            new_X.drop(columns=self.pot_leak_cols, inplace=True)
+        LOGGER.debug('DataPrepTransformer: Transforming dataset, out shape = {}, {}', new_X.shape, y.shape if y is not None else None)
+        return new_X
+
+    def get_params(self, deep=True):
+        return {
+            'date_cols': self.date_cols,
+            'int_cols': self.int_cols,
+            'float_cols': self.float_cols,
+            'masked_cols': self.masked_cols,
+            'feature_mappings': self.feature_mappings,
+            'drop_feats_cols': self.drop_feats_cols,
+            'gen_target': self.gen_target,
+            'calc_features': self.calc_features,
+            'pot_leak_cols': self.pot_leak_cols,
+        }